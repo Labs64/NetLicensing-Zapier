e6c88f837437f30d4ee5099862d9de4f5872172c
R.Brown <ready.brown@hotmail.de>
2019-07-24 16:31:10 +0200

    Update Constants.js

--- config/Constants.js
+++ config/Constants.js
@@ -8,35 +8,35 @@ module.exports = Object.freeze({
     triggers: {
         NEW_LICENSEE_NOUN: 'Licensee',
         NEW_LICENSEE_LABEL: 'New Licensee',
-        NEW_LICENSEE_DESCRIPTION: 'Triggers when a new licensee is created.',
+        NEW_LICENSEE_DESCRIPTION: 'Triggers when a new licensee is created',
 
         NEW_PRODUCT_NOUN: 'Product',
         NEW_PRODUCT_LABEL: 'New Product',
-        NEW_PRODUCT_DESCRIPTION: 'Triggers when a new product is created.',
+        NEW_PRODUCT_DESCRIPTION: 'Triggers when a new product is created',
     },
     searches: {
         FIND_LICENSEE_NOUN: 'Licensee',
         FIND_LICENSEE_LABEL: 'Find a Licensee',
-        FIND_LICENSEE_DESCRIPTION: 'Search for Licensee by number.',
+        FIND_LICENSEE_DESCRIPTION: 'Search a Licensee by its number',
 
         FIND_PRODUCT_NOUN: 'Product',
         FIND_PRODUCT_LABEL: 'Find a Product',
-        FIND_PRODUCT_DESCRIPTION: 'Search for Product by number.',
+        FIND_PRODUCT_DESCRIPTION: 'Search a Product by its number',
     },
     search_or_creates: {
-        SEARCH_OR_CREATE_LICENSEE_LABEL: 'Search Licensee or Create one',
-        SEARCH_OR_CREATE_LICENSEE_DESCRIPTION: 'If not exist then create one.',
+        SEARCH_OR_CREATE_LICENSEE_LABEL: 'Search or Create Licensee',
+        SEARCH_OR_CREATE_LICENSEE_DESCRIPTION: 'Create a Licensee if this does not exist',
 
-        SEARCH_OR_CREATE_PRODUCT_LABEL: 'Search Product or Create one',
-        SEARCH_OR_CREATE_PRODUCT_DESCRIPTION: 'If not exist then create one.',
+        SEARCH_OR_CREATE_PRODUCT_LABEL: 'Search or Create Product',
+        SEARCH_OR_CREATE_PRODUCT_DESCRIPTION: 'Create a Product if this does not exist',
     },
     creates: {
         CREATE_LICENSEE_NOUN: 'Licensee',
         CREATE_LICENSEE_LABEL: 'Create Licensee',
-        CREATE_LICENSEE_DESCRIPTION: 'Creates a new licensee.',
+        CREATE_LICENSEE_DESCRIPTION: 'Create a new Licensee',
 
         CREATE_PRODUCT_NOUN: 'Product',
         CREATE_PRODUCT_LABEL: 'Create Product',
-        CREATE_PRODUCT_DESCRIPTION: 'Creates a new product.',
+        CREATE_PRODUCT_DESCRIPTION: 'Create a new Product',
     },
 });
