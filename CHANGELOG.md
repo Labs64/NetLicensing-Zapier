185f1338b51a768074753c5bd5a20980c44e03bc
Alexey Averikhin <alexey.averikhin@labs64.de>
2019-12-08 19:41:53 +0100

    - update app's Core dependency to 9.0.0 - reorganize scripts - update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@
 ## 0.9.1
 
 * API Key authentication added
+
+## 0.9.2
+
+* Update app's Core dependency to 9.0.0
--- etc/1_zapier_init_login.sh
+++ etc/1_zapier_init_login.sh
@@ -3,5 +3,8 @@ echo 'NetLicensing / Zapier integration: initialize environment'
 
 # install the CLI globally
 npm install -g zapier-platform-cli
+rm -rf node_modules && npm i
+
+zapier test
 
 zapier login
