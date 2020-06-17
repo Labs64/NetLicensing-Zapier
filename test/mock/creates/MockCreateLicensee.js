a5bd6c7c9d47e5dea3439ce592b65ab3d6e1c837
Alexey Averikhin <alexey.averikhin@labs64.de>
2020-06-17 22:34:11 +0200

    -- refactor mock

--- test/mock/creates/MockCreateLicensee.js
+++ test/mock/creates/MockCreateLicensee.js
@@ -1,29 +1,43 @@
-const zapier = require('zapier-platform-core/index');
-
+const zapier = require('zapier-platform-core');
+const { Service } = require('netlicensing-client/dist/netlicensing-client.node');
+const AxiosMockAdapter = require('axios-mock-adapter');
 const constants = require('../../../config/Constants');
-
 const App = require('../../../index');
+const response = require('../response');
+const error = require('../error');
 
 const appTester = zapier.createAppTester(App);
 
-/*
 describe('Create Licensee', () => {
-    it('Parameter productNumber bad value', (done) => {
-        const bundle = {
-            authData: {
-                apiKey: constants.NLIC_APIKEY_TEST,
-            },
-        };
+    let baseUrl;
+    let mock;
 
-        appTester(App.creates.create_licensee.operation.perform, bundle)
-            .then(() => {
-                done();
-            })
-            .catch((error) => {
-                error.message.should.containEql('Parameter productNumber has bad value undefined');
-                done();
-            });
+    before(() => {
+        baseUrl = `${constants.BASE_HOST}${constants.BASE_PATH}`;
+    });
+
+    beforeEach(() => {
+        mock = new AxiosMockAdapter(Service.getAxiosInstance());
+    });
+
+    it('Create Licensee', async () => {
+        const licensee = { name: 'Customer One', productNumber: 'PNUMBER1', active: true };
+
+        const bundle = {
+            authData: { apiKey: constants.NLIC_APIKEY_TEST },
+            inputData: { name: licensee.name },
+            inputData: { productNumber: licensee.productNumber },
+            inputData: { active: licensee.active },
+        };
+/*
+        // configure mock for get request
+        mock.onPost(`${baseUrl}/licensee`)
+            .reply(200, response(licensee));
+
+        const results = await appTester(App.creates.create_licensee.operation.perform, bundle);
+
+        results.should.have.property('name');
+*/
     });
 
 });
-*/
--- test/mock/creates/MockCreateProduct.js
+++ test/mock/creates/MockCreateProduct.js
@@ -1,29 +1,42 @@
-const zapier = require('zapier-platform-core/index');
-
+const zapier = require('zapier-platform-core');
+const { Service } = require('netlicensing-client/dist/netlicensing-client.node');
+const AxiosMockAdapter = require('axios-mock-adapter');
 const constants = require('../../../config/Constants');
-
 const App = require('../../../index');
+const response = require('../response');
+const error = require('../error');
 
 const appTester = zapier.createAppTester(App);
 
-/*
 describe('Create Product', () => {
-    it('Parameter productNumber bad value', (done) => {
+    let baseUrl;
+    let mock;
+
+    before(() => {
+        baseUrl = `${constants.BASE_HOST}${constants.BASE_PATH}`;
+    });
+
+    beforeEach(() => {
+        mock = new AxiosMockAdapter(Service.getAxiosInstance());
+    });
+
+    it('Create Product', async () => {
+        const product = { name: 'Product One', version: '1.0', active: true };
+
         const bundle = {
-            authData: {
-                apiKey: constants.NLIC_APIKEY_TEST,
-            },
+            authData: { apiKey: constants.NLIC_APIKEY_TEST },
+            inputData: { name: product.name },
+            inputData: { version: product.version },
+            inputData: { active: product.active },
         };
 
-        appTester(App.creates.create_product.operation.perform, bundle)
-            .then(() => {
-                done();
-            })
-            .catch((error) => {
-                error.message.should.containEql('Parameter productNumber has bad value undefined');
-                done();
-            });
+        // configure mock for get request
+        mock.onPost(`${baseUrl}/product`)
+            .reply(200, response(product));
+
+        const results = await appTester(App.creates.create_product.operation.perform, bundle);
+
+        results.should.have.property('name');
     });
 
 });
-*/
